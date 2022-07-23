5886cc5e5f8f0f301a591b4e53eb79c741624ddd
Eric <ericgaona0413@gmail.com>
2022-07-23 22:11:19 +0100

    adding the html and the app.py

--- app.py
+++ app.py
@@ -1,17 +1,96 @@
 import os
-from flask import Flask
+import json
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask import render_template
+
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+
+if os.path.exists("env.py"):
+    import env
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World...."
+# app.config["MONGO_DBNAME"] =  os.getenv("DBname") 
+# app.config["MONGO_URI"] = os.getenv("URLdirection")
+# app.secret_key = os.getenv("SECRET")
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+
+def index():
+    return render_template("index.html", tasks = mongo.db.task2.find())
+   
+# User section Login Logout Profile------------------------
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        register_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register_user)
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("index"))
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for("index"))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-     app.run(host=os.environ.get('IP'),
+    app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
-    # lo de arriba es para que funcione en heroku
+            debug=False)
+    # lo de arriba es para que funcione en heroku lo de abajo para que funcione en local pero cuando cambiamos la ruta ya no funciona mas
 # if __name__ == "__main__":
 #     app.run()
